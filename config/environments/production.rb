cceb0ca38dccad399e224354f9d930c561f6d80c
Roman Khitroff <roman.khitroff@gmail.com>
2016-08-25 22:57:36 +0300

    fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,8 +66,8 @@ Rails.application.configure do
 
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
-  Rails.application.routes.default_url_options[:host] = 'romhi.ru'
-  config.action_mailer.default_url_options = { :host => 'romhi.ru' }
+  Rails.application.routes.default_url_options[:host] = 'http://198.211.121.16/'
+  config.action_mailer.default_url_options = { :host => 'http://198.211.121.16/' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = YAML.load(File.open("#{Rails.root}/config/smtp.yml"))
 
