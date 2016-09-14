f7c2b99d7257bdd98fd3bdb30e087c415662d03a
Roman Khitroff <roman.khitroff@gmail.com>
2016-09-14 17:43:39 +0300

    fix

--- app/models/volunteer.rb
+++ app/models/volunteer.rb
@@ -4,6 +4,8 @@ class Volunteer < ActiveRecord::Base
   belongs_to :responsibility
   has_one :vacancy
   scope :by_congregation_id, -> congregation_id { where("congregation_id = ?", congregation_id) }
+  # scope :without_vacancy, -> (vacancy_id){ Vacancy.find(vacancy_id).try(:volunteer).present? ? (where("id not in (?)", Vacancy.where("volunteer_id is not null").pluck(:volunteer_id)).order(:congregation_id)) : order(:congregation_id)}
+
   validates :age, numericality: { greater_than: 18}
   validates_presence_of :congregation_id, :last_name, :first_name, :age,
                         :convenient_start_time, :convenient_end_time, :will_be_since_8, :car,
@@ -15,6 +17,14 @@ class Volunteer < ActiveRecord::Base
 
   def city_and_congregation
     congregation.city.name == congregation.name ? "#{congregation.name}" : "#{congregation.city.name} #{congregation.name}"
+  end
+
+  def self.without_vacancy(vacancy_id)
+    if Vacancy.where("volunteer_id is not null").pluck(:volunteer_id).count > 0
+      self.where("id not in (?)", Vacancy.where("volunteer_id is not null").pluck(:volunteer_id))
+    else
+      self.order(:congregation_id)
+    end
   end
 
 end
